302bb2cf409c973b35bc45f81c4af63033f63a9a
Nicki Thomson <nicki.r.thomson@gmail.com>
2014-03-13 00:59:00 -0400

    Marked T stops on map

--- transit/transit.js
+++ transit/transit.js
@@ -11,10 +11,20 @@
 			var marker;
 			var infowindow = new google.maps.InfoWindow();
 			var places;
+			var tstopsData;
+			var linecolor;
+			var stations;
+			var stationsData = new XMLHttpRequest();
 
 			function init()
 			{
 				map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+				request.open("get", "latlang.json", true);
+				request.send(null);
+				request.onreadystatechange = callback;
+				//stations = JSON.parse(request.responseText);
+
 				getMyLocation();
 			}
 
@@ -45,8 +55,23 @@
 					title: "Here I Am!"
 				});
 				marker.setMap(map);
-
 				// Open info window on click of marker
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(marker.title);
+					infowindow.open(map, marker);
+				});
+				console.log('Next is get T info');
+				
+				request.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+				request.send(null);
+				request.onreadystatechange = dataReady;
+
+				//Get T info
+				/*request.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+				request.send(null);
+				callback();*/
+
+/*				// Open info window on click of marker
 				google.maps.event.addListener(marker, 'click', function() {
 					infowindow.setContent(marker.title);
 					infowindow.open(map, marker);
@@ -59,11 +84,11 @@
 					types: ['food']
 				};
 				service = new google.maps.places.PlacesService(map);
-				service.search(request, callback);
+				service.search(request, callback);*/
 			}
 
 			// Taken from http://code.google.com/apis/maps/documentation/javascript/places.html
-			function callback(results, status)
+			/*function callback(results, status)
 			{
 				if (status == google.maps.places.PlacesServiceStatus.OK) {
 					alert("Got places back!");
@@ -72,19 +97,83 @@
 						createMarker(results[i]);
 					}
 				}
-			}
+			}*/
 
-			function createMarker(place)
+	/*		function createMarker()
 			{
-				var placeLoc = place.geometry.location;
+				console.log("In createMarker");
+
+
+
 				var marker = new google.maps.Marker({
 					map: map,
-					position: place.geometry.location
-				});
+					position: 
+				}
 
 				google.maps.event.addListener(marker, 'click', function() {
 					infowindow.close();
 					infowindow.setContent(place.name);
 					infowindow.open(map, this);
+				});*/
+
+			    /*function parse()
+				{
+				request.open("GET", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
+				request.send(null);
+				request.onreadystatechange = callback;*/
+
+			function callback() 
+			{
+        		console.log("In callback");
+        		if (request.readyState == 4 && request.status == 200) 
+        		{
+           			str = request.responseText;
+           			stations = JSON.parse(str);
+//            		linecolor = data["line"];  
+       			 }
+   			 }
+
+   			 function dataReady(){
+   			 	console.log(request.readyState, request.status);
+   			 	if (request.readyState == 4 && request.status == 200) 
+        		{
+           			tstopsData = JSON.parse(request.responseText);
+//            		linecolor = data["line"];
+//            		tstopsDom = document.getElementById("schedule");
+ //           		tstopsDom.innerHTML = tstopsData["schedule"] 
+ 					markStops();
+
+
+       			 } else if (request.readyState == 4 && request.status == 500){
+       			 	console.log("ERROR-Y Stuff!");
+       			 }
+
+   			 }
+
+   			 function markStops(){
+   			 	linecolor = tstopsData['line'];
+
+   			 	for (var i=0; i<stations[linecolor].length; i++){
+   			 		var mark = new google.maps.LatLng(stations[linecolor][i]['lat'], stations[linecolor][i]['long']);
+   			 		
+   			 		marker = new google.maps.Marker({
+					position: mark,
+					title: stations[linecolor][i]['stop']
 				});
-      }
+				marker.setMap(map);
+
+	   			 	google.maps.event.addListener(mark, 'click', function() {
+						infowindow.close();
+						infowindow.setContent(stations[linecolor][i]['stop']);
+						infowindow.open(map, this);
+					});
+				}
+			}
+
+
+ 
+
+
+
+
+
